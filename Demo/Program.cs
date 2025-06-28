26bf6e65fd0a42c9550c5698f764a29c3d9d1ae5
Mena Erian Farouk <135561286+Mena-Erian@users.noreply.github.com>
2025-06-28 22:53:37 +0300

    Part 01 Conditional Statments - If, Switch [Numeric Datatypes]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Control Statments 1. Conditional Statments [if, switch]
+            
+            #region Example 01 [Numeric Type: Equality]
+            //Console.WriteLine("Please Enter an Month Numbers Exsisted in 1st Quarter: ");
+
+            //int.TryParse(Console.ReadLine(), out int monthNumber);
+
+
+            ///if (monthNumber == 1)
+            ///    Console.WriteLine("Hello january");
+            ///else if (monthNumber == 2)
+            ///    Console.WriteLine("Hello Febraury");
+            ///else if (monthNumber == 3)
+            ///    Console.WriteLine("Hello March");
+            ///else 
+            ///    Console.WriteLine("The Entered Month Numbers is not Existed in 1st Quarter");            
+
+            // Jumb Table(HashTable)=>Generated in release mode, Genereted in Compile time and you can
+            // see the jumbTable in Il but in (Release mode) 
+            // Use Switch statment when we compare with numbaric value (EQUALITY) rather than if statment
+            ////switch (monthNumber)
+            ////{
+            ////    case 1:
+            ////         Console.WriteLine("Hello january");
+            ////        break;
+            ////    case 2:
+            ////         Console.WriteLine("Hello Febraury");
+            ////        break;
+            ////    case 3:
+            ////         Console.WriteLine("Hello March");
+            ////        break;
+            ////    default:
+            ////         Console.WriteLine("The Entered Month Numbers is not Existed in 1st Quarter");
+            ////        break;
+            ////}
+
+            #endregion
+
+
+            #endregion
         }
     }
 }
